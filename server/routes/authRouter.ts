dd783d2572c35a1eafcf21b48668435bbeeb13c9
Tung Nguyen D <ndtung723@gmail.com>
2023-08-20 23:11:48 +0700

    active account

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -5,6 +5,7 @@ import { validRegister } from '../middleware/valid'
 const router = express.Router()
 
 router.post('/register', validRegister,  authCtrl.register)
+router.post('/active',  authCtrl.activeAccount)
 
 
 export default router
